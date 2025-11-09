4e724fe065ef5a430cff784d8795fff0790135ef
truongSky204 <truong1872004@gmail.com>
2025-11-09 21:25:12 +0700

    Update about_page

--- lib/ui/settings/about_page.dart
+++ lib/ui/settings/about_page.dart
@@ -25,7 +25,7 @@ class AboutPage extends StatelessWidget {
             Text(
               'Thành Viên Gồm: '
                   'LÊ XUÂN TRƯỜNG '
-                  'PHẠM DUY HOÀNG '
+                  'LÊ MINH TRƯỜNG '
                   'ứng dụng cung cấp giao diện trực quan, dễ sử dụng và nhiều tính năng thú vị như: '
                   'tìm kiếm bài hát, nghe nhạc trực tuyến, tạo playlist cá nhân, và nhiều chức năng khác.',
               style: TextStyle(color: Colors.white, fontSize: 16),
